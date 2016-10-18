39f57bd2d71bfddd1f83208d3059f266e30e74e5
feiben <feiben365@gmail.com>
2016-10-18 19:51:29 +0800

    page 2 step 5

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_filter :require_is_admin
 
   def show
     @job = Job.find(params[:id])
@@ -35,9 +36,16 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path
   end
 
+  def require_is_admin
+    if current_user.email != 'feiben365@gmail.com'
+      flash[:alert] = 'You are not admin'
+      redirect_to root_path
+    end
+  end
+
   private
 
   def job_params
     params.require(:job).permit(:title, :description)
   end
-end 
+end
