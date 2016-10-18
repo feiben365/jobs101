7f859c36a178832e46cafa9b7f920f55ef5f2bf4
feiben <feiben365@gmail.com>
2016-10-18 18:00:30 +0800

    page 1 step 9

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,10 +3,17 @@ class JobsController < ApplicationController
       @jobs =Job.all
     end
 
-
    def new
     @job = Job.new
   end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end 
 
   def create
     @job = Job.new(job_params)
@@ -16,12 +23,23 @@ class JobsController < ApplicationController
     else
       render :new
     end
-
-  def show
-    @job = Job.find(params:[:id])
   end
 
-end
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path, notice: 'Update Success'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+
+    @job.destroy
+    redirect_to jobs_path, alert: 'Group deleted'
+  end
 
 
  private
